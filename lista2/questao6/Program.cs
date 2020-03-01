dd3b7f6284e9fadf3a9d6323975ec7994bb1a7dd
Larissa Karla dos Santos Nascimento <larissa.karla@academico.ifrn.edu.br>
2020-03-01 13:13:00 -0300

    primeira versão

---
+++ lista2/questao6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três valores inteiros");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a > b && b > c)
+            {
+                int s = a + c;
+                Console.WriteLine($"A soma do maior com o menor número é {s}");
+            }
+            if (b > a && a > c)
+            {
+                int s = b + c;
+                Console.WriteLine($"A soma do maior com o menor número é {s}");
+            }
+            if (a > c && c > b)
+            {
+                int s = a + c;
+                Console.WriteLine($"A soma do maior com o menor número é {s}");
+            }
+            if (b > c && c > a)
+            {
+                int s = a + c;
+                Console.WriteLine($"A soma do maior com o menor número é {s}");
+            }
+            if (c > b && b > a)
+            {
+                int s = a + c;
+                Console.WriteLine($"A soma do maior com o menor número é {s}");
+            }
+            if (c > a && a > b)
+            {
+                int s = a + c;
+                Console.WriteLine($"A soma do maior com o menor número é {s}");
+            }
+        }
+    }
+}
